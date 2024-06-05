6f7b7fb44c090e0938fe49d2636773a605926420
seonjo1 <tjdgus9684@gmail.com>
2024-06-05 14:21:05 +0900

    sda

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -5,4 +5,24 @@ chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/local/bin/wp
 
+wp core download --path=/var/www/html --allow-root
+
+wp config create \
+    --path=/var/www/html \
+    --dbname=wordpress \
+    --dbuser=wordpress \
+    --dbpass=wordpress \
+    --dbhost=localhost \
+    --skip-check \
+    --allow-root
+
+wp core install \
+    --path=/var/www/html \
+    --url=seonjo.42.fr \
+    --title=seonjo \
+    --admin_user=seonjo \
+    --admin_password=password \
+    --admin_email=seonjo@student.42seoul.kr \
+    --allow-root
+
 exec /usr/sbin/php-fpm8.2 -F
