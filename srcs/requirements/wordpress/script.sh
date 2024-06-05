8e47b78a2048b2dc9eaa69eee8bb5d15a61c6999
seonjo1 <tjdgus9684@gmail.com>
2024-06-05 14:37:11 +0900

    ㅁㄴㅈㅇ

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -7,4 +7,13 @@ mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --path=/var/www/html --allow-root
 
+wp config create \
+    --path=/var/www/html \
+    --dbname=wordpress \
+    --dbuser=wordpress \
+    --dbpass=wordpress \
+    --dbhost=localhost \
+    --skip-check \
+    --allow-root
+
 exec /usr/sbin/php-fpm8.2 -F
