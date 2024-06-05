8aecfd7993ce8edec465fbb0bdff996ddb256804
seonjo1 <tjdgus9684@gmail.com>
2024-06-05 14:45:59 +0900

    ㄴㅇㄹㄴㅇㄹ

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -7,13 +7,13 @@ mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --path=/var/www/html --allow-root
 
-wp config create \
+wp core install \
     --path=/var/www/html \
-    --dbname=wordpress \
-    --dbuser=wordpress \
-    --dbpass=wordpress \
-    --dbhost=localhost \
-    --skip-check \
+    --url=seonjo.42.fr \
+    --title=seonjo \
+    --admin_user=seonjo \
+    --admin_password=password \
+    --admin_email=seonjo@student.42seoul.kr \
     --allow-root
 
 exec /usr/sbin/php-fpm8.2 -F
