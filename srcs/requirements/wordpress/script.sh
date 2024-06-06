01b2d379fc66092840209e92ed9e6020bc4b7a38
seonjo1 <tjdgus9684@gmail.com>
2024-06-06 16:15:52 +0900

    sdfsd

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -7,26 +7,17 @@ mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --path=/var/www/html --allow-root
 
-chmod -R 777 /var/www
 
 wp config create \
 	--force \
     --skip-check \
-	--path=/var/www/html \
     --dbname=$DB_NAME \
     --dbuser=$DB_USER \
     --dbpass=$DB_PASS \
     --dbhost=mariadb \
     --allow-root
 
-wp core install \
-    --path=/var/www/html \
-    --url=$DOMAIN \
-    --title=seonjo \
-    --admin_user=$ADMIN_USER \
-    --admin_password=$ADMIN_PASS \
-    --admin_email=$ADMIN_EMAIL \
-    --allow-root
+chmod -R 777 /var/www
 
 sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
 
