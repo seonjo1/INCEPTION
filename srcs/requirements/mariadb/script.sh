b0144d4a7be960a8f8a245af498fa7ccca964553
seonjo1 <tjdgus9684@gmail.com>
2024-06-06 14:25:32 +0900

    sdf

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,10 +1,13 @@
 service mariadb start
 
-echo "CREATE DATABASE $DB_NAME;" > db.sql
-echo "CREATE USER $DB_USER@localhost IDENTIFIED BY $DB_PASS;" >> db.sql
-echo "CREATE USER $DB_USER@% IDENTIFIED BY $DB_PASS;" >> db.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@%;" >> db.sql
-echo "FLUSH PRIVILEGES" >> db.sql
+cat <<EOF > db.sql
+CREATE DATABASE $DB_NAME;
+CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';
+CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
 
 mysql -u root < db.sql
 
