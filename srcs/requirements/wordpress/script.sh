a627af7179308d881b9c384d2ed19850a7b847a1
seonjo1 <tjdgus9684@gmail.com>
2024-06-08 17:46:04 +0900

    php 버전 7.4로 변경

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -33,8 +33,8 @@ fi
 
 chmod -R 777 /var/www
 
-sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
-echo "clear_env = no" >> /etc/php/8.2/fpm/pool.d/www.conf
+echo "clear_env = no" >> /etc/php/7.4/fpm/pool.d/www.conf
 
-exec /usr/sbin/php-fpm8.2 -F
+exec /usr/sbin/php-fpm7.4 -F
